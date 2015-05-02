50288a0ae557684b322e5138a2facaebff83cf9d
Tiago Carvalho Miranda <tgcvmr@gmail.com>
2015-05-02 12:32:32 -0300

    base commit

---
+++ http.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var request = require('request');
+var emitter = require('actor-emitter');
+var namespace = 'request';
+
+var onRequestResponse = function(cb, err, res, body) {
+
+    if (err)
+        emitter.next(onRequestError, err, res, body);
+    else
+        emitter.next(onValidateResponse, cb, err, res, body);
+}
+
+var onValidateResponse = function(cb, err, res, body) {
+
+    let key = 'request:'
+            + res.request.uri.host + ''
+            + res.request.uri.path + ':'
+            + res.statusCode ;
+
+    if (res.statusCode != 200)
+        emitter.next(onRequestError, err, res);
+
+    else (cb && setTimeout(cb.bind(this, err, res, body), 0));
+
+    console.log('<- ', key);
+    emitter.trigger(key);
+}
+
+var onRequestError = function(err, res) {
+
+    res = res || {};
+
+    console.error(err, res.statusCode);
+}
+
+var onRequestedGet = function(cb, url) {
+
+    url = typeof url == "string"
+        ? url : cb ;
+
+    console.log('-> ', 'request:' + url.split('://').pop());
+    request.get(url, onRequestResponse.bind(this, cb));
+}
+
+var onRequestedMake = function(cb, options) {
+
+    options = typeof cb == "function"
+        ? options : cb ;
+
+    console.log('-> ', 'request:' + url.split('://').pop());
+    request(options, onRequestResponse.bind(this, cb));
+}
+
+emitter.bind('request:get', onRequestedGet);
+emitter.bind('request:make', onRequestedMake);
+
+module.exports = emitter;
